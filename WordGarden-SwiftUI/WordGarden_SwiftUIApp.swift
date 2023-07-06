c8750f21960b5430451f7ccfebff45bcddcf9b40
Bob Witmer <bwitmer@golden.net>
2023-07-06 17:09:13 -0400

    Initial Commit

---
+++ WordGarden-SwiftUI/WordGarden_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGarden_SwiftUIApp.swift
+//  WordGarden-SwiftUI
+//
+//  Created by Bob Witmer on 2023-07-06.
+//
+
+import SwiftUI
+
+@main
+struct WordGarden_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
